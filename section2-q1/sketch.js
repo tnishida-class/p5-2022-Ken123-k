086b485daf84acbc7c3c0c444b28769389e67fa1
Ken123-k <kamakiri5011@gmail.com>
2022-10-17 22:26:08 +0900

    Change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
-// 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  for(let i = 0; i < 11; i++){
+    ellipse(50, 50, 8*i);
+
+    if(i < 5){
+      stroke(0, 0, 255);
+    }
+    
+    else{
+      stroke(255, 0, 0);
+  }
+    
+      
+    noFill();           
   }
 }
