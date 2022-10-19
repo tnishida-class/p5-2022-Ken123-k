8ee3f697fe991fc55a0e99debbe7905fd29191ca
Ken123-k <kamakiri5011@gmail.com>
2022-10-19 23:09:04 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,25 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let s = 2; s++
 
 function setup(){
+ 
   createCanvas(200, 200);
+   fill(191 , 30, 51);
   count = 0;
-  cycle = 100;
+  cycle = 40;
+  x = width / 2;
+  y = height / 2;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  let size = count + 60;
+  if(keyIsDown("A".charCodeAt(0))){ count = (count + s) % cycle; } 
+  
+ 
+
+  ellipse(x, y, size);
 }
